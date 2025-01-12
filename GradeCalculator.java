9b1dade89e4340cafa32fcd45fcc3d41f3672be4
karkibj <karkibj341@gmail.com>
2025-01-12 07:40:52 +0545

    Added gradeCalculator.java

---
+++ GradeCalculator.java
@@ -0,0 +1,18 @@
+public class GradeCalculator {
+    // Method to calculate grade based on marks
+    public String calculateGrade(double marks) {
+        if (marks >= 90) {
+            return "A+";
+        } else if (marks >= 80) {
+            return "A";
+        } else if (marks >= 70) {
+            return "B+";
+        } else if (marks >= 60) {
+            return "B";
+        } else if (marks >= 50) {
+            return "C";
+        } else {
+            return "F";
+        }
+    }
+}
